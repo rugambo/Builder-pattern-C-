24285972d128fb403c9edfa921a670bfc1694eb2
Alex Rugama <alrugama@ucsc.edu>
2024-09-09 19:11:05 -0700

    Add project files.

---
+++ Builder pattern C#/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder_pattern_C_
+{
+    internal class Player
+    {
+        //Test name, level, and bool
+        public string Name;
+        public int Level;
+        public bool joinedGame;
+
+        public Player(string _Name)
+        {
+            Name = _Name;
+            Level = 0;  
+            joinedGame = false;
+        }
+
+        public string CreatePlayerDescription()
+        {
+            string description = "This is named: " + Name;
+            return description; 
+        }
+
+    }
+}
---
+++ Builder pattern C#/Program.cs
@@ -0,0 +1,13 @@
+﻿using Builder_pattern_C_;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Player playerObject = new Player("player_1");
+        Player enemyObject = new Player("enemy_1");
+        //Console.WriteLine("Hello, World!");
+        Console.WriteLine(playerObject.CreatePlayerDescription());
+        Console.WriteLine(enemyObject.CreatePlayerDescription());
+    }
+}
